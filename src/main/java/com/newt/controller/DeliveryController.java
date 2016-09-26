1de7e220de8d4e5da0f68dc03e9d52f20314f55d
Kumarswamy Chinthala <kumarc@newtglobal.chn.local>
2016-09-26 11:19:21 +0530

    first commit

--- src/main/java/com/newt/controller/DeliveryController.java
+++ src/main/java/com/newt/controller/DeliveryController.java
@@ -3,6 +3,8 @@ package com.newt.controller;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.newt.model.Orders;
 import com.newt.repository.DeliveryRepository; 
 import com.wordnik.swagger.annotations.ApiOperation;
+ 
 
 @RestController
 @RequestMapping("/delivery")
@@ -19,35 +22,25 @@ public class DeliveryController {
 	@Autowired
 	private DeliveryRepository orderRepository;
 	@Autowired
-    private Notifications notifications;
-	
-/*	
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	@ApiOperation(value = "get orders1")
-    public Orders find(@PathVariable Integer id) {
-           return orderRepository.findOne(id);        
-    }
-	*/
-   
-	
-	@PUT
-	@Produces(MediaType.APPLICATION_JSON)
-	@RequestMapping("update/{id}/{orderStatus}")
-	@ApiOperation(value = "put orders")
+    private Notifications notifications; 
+	private static final Logger logger = Logger.getLogger(DeliveryController.class);	
+	 
+	@RequestMapping(value = "/{id}/{orderStatus}" ,method = RequestMethod.PUT)
+	@ApiOperation(value = "delivery orders")
 	public Orders deliverOrder(@PathVariable("id") int id,@PathVariable("orderStatus") String orderStatus){
-		System.out.println("Updating Orderstatus for " + id);
+		logger.info("Updating Orderstatus for " + id);
 	    
 		Orders order = orderRepository.findOrdersByorderId(id);
 		try{
 	    if(order.getOrderId()==id){
-	    	System.out.println("Orders is  Id is  found");
+	    	logger.info("Orders is  Id is  found");
 	    	if(order.getOrderStatus().equalsIgnoreCase("shipped")){
 	    		 order.setOrderStatus("delivered");
 	    		 orderRepository.save(order);
 	    	}else if(!(order.getOrderStatus().equalsIgnoreCase("shipped"))){
 	    			//sent mail to user
 	    			  notifications.sendNotification("order is not delivered some isuues are happened");
-		    			System.out.println("Order is not shipped....");
+	    			  logger.info("Order is not shipped....");
 	    			 
 	    		}
 	    	 
@@ -57,7 +50,7 @@ public class DeliveryController {
 	 
 	else{
 	          	
-		  System.out.println("Order is not found..");
+		logger.info("Order is not found..");
 		}
 	}catch(Exception e){
 		e.printStackTrace();
